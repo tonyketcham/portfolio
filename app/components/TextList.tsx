3f1c278219aa8e683feeaf03db4524821998d52b
Tony Ketcham <tony@modyfi.io>
2025-02-11 16:41:29 -0800

    fine tune decorator spacing

--- app/components/TextList.tsx
+++ app/components/TextList.tsx
@@ -4,4 +4,4 @@ export const TextList = twc.ul`w-fit text-rock-gray-400`;
 
 export const TextListItem = twc.li`flex gap-2`;
 
-export const ListDecorator = twc.span`inline-block w-5 select-none`;
+export const ListDecorator = twc.span`inline-block w-5 select-none tracking-[-3px]`;
